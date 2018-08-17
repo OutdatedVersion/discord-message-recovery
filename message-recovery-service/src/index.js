0b710859aa5ff0e1f836b46ac009d361c79ccc39
Ben Watkins <ben@outdatedversion.com>
2018-08-17 02:03:30 -0500

    Have the API serve the hostname for testing

--- message-recovery-service/src/index.js
+++ message-recovery-service/src/index.js
@@ -1,12 +1,16 @@
 import Koa from 'koa'
 import Router from 'koa-router'
 import { logging as log } from 'common'
+import { execSync } from 'child_process';
 
 const koa = new Koa()
 const router = new Router()
 
+// For testing
+const hostname = execSync("hostname").toString()
+
 router.get("/message/:guildID", async (context) => {
-    
+    context.body = hostname
 })
 
 router.post("/message/:guildID", async (context) => {
