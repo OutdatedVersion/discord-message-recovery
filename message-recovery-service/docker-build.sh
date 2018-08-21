818002ec313d1ac32e01e4dd5a24d2aa9e2f5884
Ben Watkins <ben@outdatedversion.com>
2018-08-20 21:27:50 -0500

    (message-recovery-service): Actually build the source...

--- message-recovery-service/docker-build.sh
+++ message-recovery-service/docker-build.sh
@@ -6,14 +6,10 @@ registryURL="docker-registry.outdatedversion.com"
 
 echo "Version: $version"
 
-echo "Grabbing common modules"
-
-cp -r ../common .
+echo "Building Source"
+yarn build
 
 echo "Building Image"
 sudo docker build -t $imageName .
 sudo docker tag $imageName $registryURL/$imageName
 sudo docker push $registryURL/$imageName
-
-echo "Removing common modules"
-rm -rf common
