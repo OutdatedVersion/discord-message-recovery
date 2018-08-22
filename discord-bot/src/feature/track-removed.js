84c6450fa3a1785151a78d527ce4cacf8ceab133
Ben Watkins <ben@outdatedversion.com>
2018-08-22 03:24:54 -0500

    (discord-bot): WIP of implementing the message recovery API

--- discord-bot/src/feature/track-removed.js
+++ discord-bot/src/feature/track-removed.js
@@ -3,20 +3,58 @@ import log from '@kratos/logging'
 import { formatName } from '../utility/user'
 import { Command, hasCommand } from '../command'
 import { distanceInWords } from 'date-fns'
+import request from 'request-promise-native'
 
+/**
+ * A URL template at which media from messages may be found.
+ */
+const STORAGE_URL_BASE = 'https://kratos.outdatedversion.com/storage/message-recovery-media'
 
 client.on('messageDelete', async message => {
-    const { content, channel, author } = message
+    const { cleanContent, channel, author } = message
 
     // do not save messages from the bot or command execution
-    if (author.id == client.user.id || hasCommand(content))
+    if (!message.guild || author.id == client.user.id || hasCommand(cleanContent))
         return
 
     const from = formatName(author)
 
-    // 
+    const body = {
+        content: cleanContent,
+        discordChannelID: channel.id,
+        discordMessageID: message.id,
+        discordGuildID: message.guild.id,
+        sentByDiscordID: author.id,
+        sentAt: message.createdTimestamp,
+        removedAt: Date.now()
+    }
 
-    log.info(`saved message from ${from} - ${content}`)
+    const mediaURLs = [], { attachments } = message
+
+    if (attachments)
+        attachments.forEach(attachment => mediaURLs.push(attachment.proxyURL))
+
+    if (mediaURLs.length)
+        body.media = mediaURLs
+
+    console.log(body)
+
+    if (true)
+        return
+
+    try {
+        await request({
+            method: 'POST',
+            uri: '',
+            body,
+            json: true
+        })
+
+        log.info(`saved message from ${from} - ${content}`)
+    }
+    catch (error) {
+        console.error('oh poop', error.stack)
+    }
 })
 
 
@@ -25,17 +63,87 @@ export const command = new Command(
     async (message, args) => {
         const limit = parseInt(args[0]) || 5
 
-        // 
+        const { guild } = message
 
-        const { length } = results
+        if (!guild) {
+            await message.reply(`You must be in a guild when using that`)
+            return
+        }
 
-        results = results.map(result => {
-            const date = distanceInWords((new Date).getTime(), result.at, { addSuffix: true })
+        const guildID = guild.id
 
-            return `:white_small_square: ${date} ${result.from} said '${result.content}'`
-        }).join('\n')
+        async function notifyOfFailure() {
+            await message.reply(`I had an issue grabbing the latest messages ):`)
+        }
 
-        await message.reply(`here is the **${length}** previously removed message${length > 1 ? 's' : ''} for you:\n${results}`)
-        message.delete()
+        try {
+            // kubernetes: http://message-recovery-api/...
+            const response = JSON.parse(await request(`http://kratos.local.dev.outdatedversion.com/api/message-recovery/message/${guildID}`))
+
+            if (!response.success) {
+                await notifyOfFailure()
+                return
+            }
+
+            await message.reply(await createDiscordMessage(response.result))
+        }
+        catch (error) {
+            await notifyOfFailure()
+            console.error(error)
+        }
+
+        await message.delete()
     }
-) 
+)
+
+async function createDiscordMessage(messages) {
+    if (messages.length == 0) {
+        return 'looks like there are no removed messages!'
+    }
+
+    const discordMessage = [getStartingLine(messages), '']
+
+    for (const message of messages) {
+        const humanRemovedAt = distanceInWords(new Date(), message.removed_at, { addSuffix: true })
+
+        // WIP
+        console.log(message.sent_by_discord_id)
+        const sentBy = await client.fetchUser(message.sent_by_discord_id)
+        // ${formatName(sentBy)}
+
+        discordMessage.push(`:writing_hand: ${humanRemovedAt}, name`)
+
+        if (message.content)
+            discordMessage.push('```' + message.content + '```')
+
+        if (message.media)
+            discordMessage.push(getMediaMessage(message))
+
+        discordMessage.push('')
+    }
+
+    return discordMessage.join('\n')
+}
+
+function getMediaMessage(message) {
+    const mediaMessage = []
+
+    for (const item of message.media) {
+        const url = formatMediaURL(message.discord_channel_id, message.discord_message_id, item.name, item.type)
+
+        // TODO(ben): have emoji dependant on media type
+        mediaMessage.push(`   :frame_photo: <${url}>`)
+    }
+
+    return mediaMessage
+}
+
+function getStartingLine(messages) {
+    return messages.length > 1
+           ? `here are the ${messages.length} removed messages for you:`
+           : 'here is the last removed message for you:'
+}
+
+function formatMediaURL(discordChannelID, discordMessageID, fileName, fileType) {
+    return `${STORAGE_URL_BASE}/${discordChannelID}/${discordMessageID}/${fileName}.${fileType}`
+}
