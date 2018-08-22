ad9ec747ebfb7a285e0efab822418717d5a297f5
Ben Watkins <ben@outdatedversion.com>
2018-08-22 01:30:47 -0500

    (discord-bot): Rewrite Dockerfile and add build script

---
+++ discord-bot/docker-build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+VERSION_FILE="build/version.json"
+
+version=$(git log -1 --format='format:%H' HEAD -- $PWD | head -c 8)
+imageName="kratos/discord-bot:$version"
+registryURL="docker-registry.outdatedversion.com"
+
+echo "Version: $version"
+
+echo "Building Source"
+yarn build
+
+echo "{\"version\": \"$version\"}" > $VERSION_FILE
+
+echo "Building Image"
+sudo docker build -t $imageName .
+sudo docker tag $imageName $registryURL/$imageName
+sudo docker push $registryURL/$imageName
