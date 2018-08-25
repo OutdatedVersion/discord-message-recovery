9d92f1127aff3a4c9433d020ca302d6b844a513c
Ben Watkins <ben@outdatedversion.com>
2018-08-25 16:21:26 -0500

    Reformat info message

--- discord-bot/src/command/information.js
+++ discord-bot/src/command/information.js
@@ -1,10 +1,9 @@
 import { Command } from '.'
-import { execSync } from 'child_process'
 import version from '../version'
 
 export default new Command(
     ['v', 'version', 'information', 'info'],
     async (message, args) => {
-        await message.reply(`**Kratos** (v${version})\n\nHey, did you know that Kratos is open-source? https://github.com/OutdatedVersion/kratos (:`)
+        await message.reply(`**Kratos** (git-${version})\n\nHey, did you know that Kratos is open-source? https://github.com/OutdatedVersion/kratos (:`)
     }
 )
