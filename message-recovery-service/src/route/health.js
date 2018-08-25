2bd6fd5bdbfb23b43477af8b140fd023f4a2af29
Ben Watkins <ben@outdatedversion.com>
2018-08-25 15:03:10 -0500

    Remove debug line

--- message-recovery-service/src/route/health.js
+++ message-recovery-service/src/route/health.js
@@ -12,8 +12,6 @@ export class HealthRoute extends RouteDefinition {
     }
 
     async handle(context) {
-        console.log('??')
-        
         const client = await captureClient()
 
         try {
