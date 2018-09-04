7e530c0bd3744768d86394062857e5e8a2f1cb83
Ben Watkins <ben@outdatedversion.com>
2018-09-04 00:13:50 -0500

    Use hard coded values for Minio client (dev)

--- message-recovery-service/src/storage/minio.ts
+++ message-recovery-service/src/storage/minio.ts
@@ -3,11 +3,11 @@ import { Client } from 'minio'
 // Create the client and expose it
 
 const client = new Client({
-    endPoint: process.env.MINIO_HOST,
+    endPoint: 'localhost',
     secure: false,
-    port: parseInt(process.env.MINIO_PORT),
-    accessKey: process.env.MINIO_ACCESS_KEY,
-    secretKey: process.env.MINIO_SECRET_KEY
+    port: 9000,
+    accessKey: 'minio',
+    secretKey: 'minio123'
 })
 
 export default client
