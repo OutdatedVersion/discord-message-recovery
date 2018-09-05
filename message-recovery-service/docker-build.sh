c2d2f57ffcf8241ce508d9a0cec7e125a386825e
Ben Watkins <ben@outdatedversion.com>
2018-09-05 00:28:08 -0500

    Add Docker build configurations

---
+++ message-recovery-service/docker-build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+REGISTRY_URL="gcr.io/$(gcloud config get-value project -q)"
+VERSION_FILE="build/version.json"
+
+version=$(git log -1 --format='format:%H' HEAD -- $PWD | head -c 8)
+imageName="message-recovery-service:$version"
+
+echo "Version: $version"
+
+echo "Building Source"
+yarn -s run build --project tsconfig.prod.json
+
+echo "{\"version\": \"$version\"}" > $VERSION_FILE
+
+echo "Building Image"
+sudo docker build -t $REGISTRY_URL/$imageName .
+sudo docker push $REGISTRY_URL/$imageName
+
+echo "Pushed $imageName to $REGISTRY_URL"
