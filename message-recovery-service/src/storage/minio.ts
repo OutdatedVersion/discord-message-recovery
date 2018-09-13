d7cc464c32868b2af01f355069e62041576a8b1d
Ben Watkins <ben@outdatedversion.com>
2018-09-12 20:56:55 -0500

    Instruct Minio client to use environment variables

--- message-recovery-service/src/storage/minio.ts
+++ message-recovery-service/src/storage/minio.ts
@@ -3,13 +3,14 @@ import { Client } from 'minio'
 // Create the client and expose it
 
 const client = new Client({
-    endPoint: 'localhost',
-    // The 'secure' option is deprecated; it will alter the intended behavior of the application.
+    endPoint: process.env.MINIO_HOST,
+    // useSSL is the up-to-date flag though TypeScript believes 'secure' is still in use.
+    // I submitted a PR to fix this; just waiting for it to propagate out.
     // @ts-ignore
     useSSL: false,
-    port: 9000,
-    accessKey: 'minio',
-    secretKey: 'minio123'
+    port: parseInt(process.env.MINIO_PORT),
+    accessKey: process.env.MINIO_ACCESS_KEY,
+    secretKey: process.env.MINIO_SECRET_KEY
 })
 
 export default client
