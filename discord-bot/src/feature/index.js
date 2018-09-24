b4c3fab4a22f3cf65164e717ad8f20e60d32c3bc
Ben Watkins <ben@outdatedversion.com>
2018-09-23 20:57:13 -0500

    Remove previous implementation

--- discord-bot/src/feature/index.js
+++
@@ -1,9 +0,0 @@
-import { registerCommand } from '../command'
-import { traverseDirectory } from '../utility/folder'
-
-export function setup(client) {
-    traverseDirectory(__dirname, (val, name) => {
-        if (name.match(/command/i))
-            registerCommand(val)
-    })
-}
